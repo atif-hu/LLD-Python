a0c7cd97cff3a1d4a0d06ecce168074f3908255b
Atif Hussain <atifhussain28@gmail.com>
2023-05-16 23:38:04 +0530

    Created Singleton 2

---
+++ Design Patterns/Singleton2_DP.py
@@ -0,0 +1,10 @@
+'''Method 2
+__new__ is also sort of constructor in python,it is a method which is also get called when we create the object
+'''
+
+class Singleton:
+    __isInstance=None
+
+    # def __new__(cls):
+    #     if __isinstance==None:
+            
